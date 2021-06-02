38bb580151ea560cad293b338ffdb8717dec4a4f
Dilaro <fatekh.bakur@gmail.com>
2021-06-02 18:52:15 +0300

    Made new url "polls". Migrate all files

--- myfirst/myfirst/urls.py
+++ myfirst/myfirst/urls.py
@@ -3,5 +3,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('articles/', include('articles.urls')),
+    path('polls/', include('polls.urls')),
     path('admin/', admin.site.urls),
 ]
