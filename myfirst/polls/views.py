6d4fc52fc51c410a77db0386bab740b810fea418
Dilaro <fatekh.bakur@gmail.com>
2021-06-03 17:14:24 +0300

    Added urls, views for polls. Corrected database

--- myfirst/polls/views.py
+++ myfirst/polls/views.py
@@ -1,6 +1,19 @@
+from polls.models import Question
 from django.http.response import HttpResponse
 from django.shortcuts import render
 
 
 def noname(request):
-    return HttpResponse('Check this page')
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    output = ', '.join([q.question_text for q in latest_question_list])
+    return HttpResponse(output)
+
+def detail(request, question_id):
+    return HttpResponse("You're looking at question %s." % question_id)
+
+def results(request, question_id):
+    response = 'You are lookimg at questions %s.'
+    return HttpResponse(response % question_id)
+
+def vote(request, question_id):
+    return HttpResponse("You're voting on question %s." % question_id)
