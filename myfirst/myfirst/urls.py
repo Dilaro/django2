df84b376c0ec9416d09bbd91f8cda3497562f8f7
Dilaro <fatekh.bakur@gmail.com>
2021-06-06 23:28:43 +0300

    Mini updates

--- myfirst/myfirst/urls.py
+++ myfirst/myfirst/urls.py
@@ -3,7 +3,6 @@ from django.urls import path, include
 from django.views.generic.base import TemplateView
 
 urlpatterns = [
-    path('articles/', include('articles.urls')),
     path('blog/', include('blog.urls')),
     path('polls/', include('polls.urls')),
     path('admin/', admin.site.urls),
